8cc8c31d687971cb4abc79ccdd62db8a0ec74951
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-12-07 19:27:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/citylife/model/Profile.kt
@@ -0,0 +1,3 @@
+package com.example.citylife.model
+
+class Profile(val hashCodeProfile: String) {}
---
+++ app/src/main/java/com/example/citylife/signupVerification/SignUpVerification.kt
@@ -0,0 +1,5 @@
+package com.example.citylife.signupVerification
+
+import java.util.Date
+
+data class SignUpVerification(val name: String, val surname: String, val birthDate: Date, val birthCity: String, val birthCountry: String) {}
