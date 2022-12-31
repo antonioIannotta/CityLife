dc491a98c0b60c53e0159dec427ab2883a204990
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2022-12-31 16:36:57 +0100

    commit

--- app/src/main/java/com/example/citylife/signUp/SignUp.kt
+++ app/src/main/java/com/example/citylife/signUp/SignUp.kt
@@ -61,20 +61,25 @@ data class SignUp(val name: String, val surname: String,
 
         client.post {
             url {
-                protocol = URLProtocol.HTTPS
+                protocol = URLProtocol.HTTP
                 host = "10.0.2.2"
                 port = 5000
                 path("/users/insertUser")
+                parameters.append("name", userDB.name)
+                parameters.append("surname", userDB.surname)
+                parameters.append("username", userDB.username)
+                parameters.append("email", userDB.email)
+                parameters.append("password", userDB.password)
+                parameters.append("distance", userDB.distance)
+                parameters.append("location", userDB.location)
+                parameters.append("reportPreference", userDB.reportPreference)
             }
-            contentType(ContentType.Application.Json)
-            setBody(userDB)
         }
 
         client.get {
             url {
-                protocol = URLProtocol.HTTPS
-                host = "10.0.2.2"
-                port = 5000
+                protocol = URLProtocol.HTTP
+                host = "10.0.2.2:5000"
                 path("/location/insertLocationAndDistance")
                 parameters.append("username", locationDB.username)
                 parameters.append("location", locationDB.location)
