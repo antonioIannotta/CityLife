b589e2f7363254c47d81489124b38fc1bb4d53a9
cataniandrea <91258294+acataniUNIBO@users.noreply.github.com>
2023-01-02 17:01:38 +0100

    commit

--- app/src/main/java/com/example/citylife/signUp/SignUp.kt
+++ app/src/main/java/com/example/citylife/signUp/SignUp.kt
@@ -7,17 +7,15 @@ import com.example.citylife.model.report.ReportType
 import com.example.citylife.model.user.User
 import com.example.citylife.http.models.UserDB
 import com.example.citylife.http.models.LocationDB
+import com.example.citylife.httpHandler.HttpHandler
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.http.ContentType.Application.Json
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.util.network.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.serializer
 import java.security.MessageDigest
 
 data class SignUp(val name: String, val surname: String,
@@ -29,14 +27,8 @@ data class SignUp(val name: String, val surname: String,
     private val username =  MessageDigest.getInstance("MD5")
         .digest((name + surname + email).toByteArray()).toString()
 
-    private val client = HttpClient(CIO) {
-        install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                isLenient = true
-            })
-        }
-    }
+    val httpHandlerReference: HttpHandler = HttpHandler()
+
     var userDB = UserDB(
         name,
         surname,
@@ -53,54 +45,36 @@ data class SignUp(val name: String, val surname: String,
         ""
     )
 
+
+    /* TODO: non viene inserito "reportPreference" nel db, dipende da chiamata app o da web service? */
     /**
-     *Funzioen che si occupa della registrazione e memorizzazione dei dati nel DB
+     *Funzione che si occupa della registrazione e memorizzazione dei dati nel DB
      */
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun signUp(): User {
 
-        /*client.get {
+        httpHandlerReference.getClient().post {
             url {
                 protocol = URLProtocol.HTTP
-                host = "10.0.2.2"
-                port = 5000
-                path("/users/insertUser")
-                parameters.append("name", userDB.name)
-                parameters.append("surname", userDB.surname)
-                parameters.append("username", userDB.username)
-                parameters.append("email", userDB.email)
-                parameters.append("password", userDB.password)
-                parameters.append("distance", userDB.distance)
-                parameters.append("location", userDB.location)
-                parameters.append("reportPreference", userDB.reportPreference)
-            }
-        }*/
-
-        client.post {
-            url {
-                protocol = URLProtocol.HTTP
-                host = "10.0.2.2"
-                port = 5000
+                host = httpHandlerReference.getHost()
+                port = httpHandlerReference.getPort()
                 path("/users/insertUser")
             }
             contentType(ContentType.Application.Json)
             setBody(userDB)
-
         }
 
-        client.get {
+        httpHandlerReference.getClient().get {
             url {
                 protocol = URLProtocol.HTTP
-                host = "10.0.2.2"
-                port = 5000
+                host = httpHandlerReference.getHost()
+                port = httpHandlerReference.getPort()
                 path("/location/insertLocationAndDistance")
                 parameters.append("username", locationDB.username)
                 parameters.append("location", locationDB.location)
                 parameters.append("distance", locationDB.distance)
             }
         }
-
-        println("insert user finito")
 
         return User(username, 0.0f, Location(""),
             emptyList<ReportType>().toMutableList())
